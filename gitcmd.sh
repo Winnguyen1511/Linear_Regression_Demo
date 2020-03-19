aba2da9264f0adc55bebafac2e8fcc0f2e2bb693
Winnguyen <khoanguyen1507dn@gmail.com>
2020-03-19 20:22:46 +0700

    Init repos

--- gitcmd.sh
+++ gitcmd.sh
@@ -5,7 +5,7 @@ api=https://api.github.com/user/repos
 reponame="Linear_Regression_Demo"
 private="false"
 gitdir=git@github.com:$user/$reponame.git
-curl -u $user $api -d '{"name":`$reponame`}'
+curl -u $user $api -d '{"name":"Linear_Regression_Demo"}'
 
 git init
 git add .
